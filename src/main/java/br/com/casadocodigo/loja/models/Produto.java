dc98c20f22a092bbc682edd0e313e8f2aedea2f3
Lauro Chicorski <laurocoelhoc@gmail.com>
2016-10-02 20:03:47 -0300

    finalizado aula3

--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +17,9 @@ public class Produto {
 	private String titulo;
 	private String descricao;
 	private int paginas;
+	
+	@ElementCollection
+	private List<Preco> precos;
 
 	public long getId() {
 		return id;
@@ -45,6 +51,14 @@ public class Produto {
 
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
+	}
+
+	public List<Preco> getPrecos() {
+		return precos;
+	}
+
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
 	}
 
 	@Override
--- src/main/webapp/WEB-INF/views/produtos/form.jsp
+++ src/main/webapp/WEB-INF/views/produtos/form.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPEs>
 <html>
 <head>
@@ -20,6 +21,13 @@
 			<label>Páginas</label>
 			<input type="text" name="paginas">
 		</div>
+		<c:forEach items="${tipos }" var="tipo" varStatus="status">
+			<div>
+				<label>${tipo }</label>
+				<input type="text" name="precos[${status.index}].valor">
+				<input type="hidden" name="precos[${status.index}].tipo" value="${tipo }">
+			</div>
+		</c:forEach>
 		<button type="submit">Cadastrar</button>
 	</form>
 </body>
