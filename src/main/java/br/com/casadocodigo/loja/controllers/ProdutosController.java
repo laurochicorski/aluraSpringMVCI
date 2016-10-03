5322919c717700163914cef1173c5851d26f80d8
Lauro Chicorski <laurocoelhoc@gmail.com>
2016-10-03 07:34:36 -0300

    finalizado aula5

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
@@ -28,10 +29,12 @@ public class ProdutosController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public String gravar(Produto produto){
+	public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes){
 		produtoDao.gravar(produto);
 		System.out.println(produto);
-		return "produtos/ok";
+		ModelAndView modelAndView = new ModelAndView("redirect:produtos");
+		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
+		return modelAndView;
 	}
 	
 	@RequestMapping(method=RequestMethod.GET)
